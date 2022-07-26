5dfaba2e7369bf12b6174710cb0bae9893a2fd41
yotamBalilti <yotam.balilti@gmail.com>
2022-07-26 13:54:11 +0300

    app ts basic

---
+++ server/src/app.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api/v1', require('./src/routes'));
+
+module.exports = app;
