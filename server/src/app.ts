0a819949254e2ac691fa7a345c576afbd737c76f
yotamBalilti <yotam.balilti@gmail.com>
2022-07-27 13:43:42 +0300

    addded configurations and db connection

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,18 +1,26 @@
 import express from 'express';
-import path from 'path';
-import cookieParser from 'cookie-parser';
-import logger from 'morgan';
 import cors from 'cors';
+import dotenv from 'dotenv';
+import connectDB from './config/db';
+
+// import path from 'path';
+// import cookieParser from 'cookie-parser';
+// import logger from 'morgan';
 
 const app = express();
 
+connectDB();
+
 app.use(cors());
-app.use(logger('dev'));
+
+// app.use(logger('dev'));
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
 
-app.use('/api/v1', require('./src/routes'));
+// app.use(express.static(path.join(__dirname, 'public')));
 
-module.exports = app;
+// app.use('/api/v1', require('./src/routes'));
+
+export default app;
